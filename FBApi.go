82bf02ca138a653f77c11a841f7509a89976061e
Arjun <muddana.m@gmail.com>
2022-06-06 12:21:04 +0400

    Update FBApi.go

--- FBApi.go
+++ FBApi.go
@@ -1,22 +1,51 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
+// A Response struct to map the Entire Response
+type Response struct {
+	Name    string    `json:"name"`
+	Pokemon []Pokemon `json:"pokemon_entries"`
+}
+
+// A Pokemon Struct to map every pokemon to.
+type Pokemon struct {
+	EntryNo int            `json:"entry_number"`
+	Species PokemonSpecies `json:"pokemon_species"`
+}
+
+// A struct to map our Pokemon's Species which includes it's name
+type PokemonSpecies struct {
+	Name string `json:"name"`
+}
+
 func main() {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
-	//We Read the response body on the line below.
-	body, err := ioutil.ReadAll(resp.Body)
+
+	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
-	//Convert the body to type string
-	sb := string(body)
-	log.Printf(sb)
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Println(responseObject.Name)
+	fmt.Println(len(responseObject.Pokemon))
+
+	for i := 0; i < len(responseObject.Pokemon); i++ {
+		fmt.Println(responseObject.Pokemon[i].Species.Name)
+	}
+
 }
